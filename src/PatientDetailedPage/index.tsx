58dd29d29b69b3bd15ce86e8e2be6bc57a00cd9d
Yusif Salam-zade <yusalam@gmail.com>
2020-09-10 13:54:26 +0300

    9.17

--- src/PatientDetailedPage/index.tsx
+++ src/PatientDetailedPage/index.tsx
@@ -4,24 +4,31 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { apiBaseUrl } from "../constants";
 import { Patient } from "../types";
+import { useStateValue } from "../state";
 
 const PatientDetailedPage: React.FC = () => {
     const [patient, setPatient] = useState<Patient | undefined>(undefined);
+    const [{ patientsSensitive }, dispatch] = useStateValue();
     const { id } = useParams<{ id: string }>();
     useEffect(() => {
         const getPatientInfo = async () => {
             try {
-                const patientDataPromise = await axios.get(
-                    `${apiBaseUrl}/patients/${id}`
-                );
-                const patientData = await patientDataPromise.data;
-                setPatient(patientData);
+                if (patientsSensitive[id]) {
+                    setPatient(patientsSensitive[id]);
+                } else {
+                    const patientDataPromise = await axios.get(
+                        `${apiBaseUrl}/patients/${id}`
+                    );
+                    const patientData = await patientDataPromise.data;
+                    dispatch({ type: "MODIFY_PATIENT", payload: patientData });
+                    setPatient(patientData);
+                }
             } catch (e) {
                 console.error(e.response.data);
             }
         };
         getPatientInfo();
-    }, [id]);
+    }, [id, dispatch, patientsSensitive]);
 
     return (
         <div>
--- src/state/state.tsx
+++ src/state/state.tsx
@@ -4,32 +4,34 @@ import { Patient } from "../types";
 import { Action } from "./reducer";
 
 export type State = {
-  patients: { [id: string]: Patient };
+    patients: { [id: string]: Patient };
+    patientsSensitive: { [id: string]: Patient };
 };
 
 const initialState: State = {
-  patients: {}
+    patients: {},
+    patientsSensitive: {},
 };
 
 export const StateContext = createContext<[State, React.Dispatch<Action>]>([
-  initialState,
-  () => initialState
+    initialState,
+    () => initialState,
 ]);
 
 type StateProviderProps = {
-  reducer: React.Reducer<State, Action>;
-  children: React.ReactElement;
+    reducer: React.Reducer<State, Action>;
+    children: React.ReactElement;
 };
 
 export const StateProvider: React.FC<StateProviderProps> = ({
-  reducer,
-  children
+    reducer,
+    children,
 }: StateProviderProps) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <StateContext.Provider value={[state, dispatch]}>
-      {children}
-    </StateContext.Provider>
-  );
+    const [state, dispatch] = useReducer(reducer, initialState);
+    return (
+        <StateContext.Provider value={[state, dispatch]}>
+            {children}
+        </StateContext.Provider>
+    );
 };
 export const useStateValue = () => useContext(StateContext);
